947d770124b0f606ce8bdc93ddcf8a3984e96203
theburningmonk <theburningmonk@gmail.com>
2013-07-21 01:37:44 +0100

    - split up the simple read workflow example from the TCP example

--- BitReaderWriterFs/BitReaderWriterFs.Examples/ParseTcp.fsx
+++ BitReaderWriterFs/BitReaderWriterFs.Examples/ParseTcp.fsx
@@ -7,27 +7,6 @@ open System.IO
 open System.Collections
 open System.Collections.Generic
 
-let stream = new MemoryStream()
-stream.WriteByte(255uy)
-stream.WriteByte(0uy)
-stream.WriteByte(129uy)
-stream.Write(Text.Encoding.ASCII.GetBytes("Hello World!"), 0, 12)
-
-stream.Position <- 0L
-
-let allT, allF, t, sixFOneT, bytes, world, ``!`` = bitReader stream {
-        let! allT     = BitReader.ReadInt32(4)
-        do! BitReader.Skip(8)
-        let! allF     = BitReader.ReadInt32(4)
-        let! t        = BitReader.ReadBool()
-        let! sixFOneT = BitReader.ReadInt32(7)
-        let! bytes'   = BitReader.ReadBytes(8*6)
-        let! world    = BitReader.ReadString(5 * 8)
-        let! ``!``    = BitReader.ReadChar()
-
-        return allT, allF, t, sixFOneT, bytes', world, ``!``
-    }
-
 let outStream = new MemoryStream()
 
 let writeTcpHeaders (stream : Stream) =
@@ -93,4 +72,3 @@ let winSize, chkSum, urgPtr, payload = bitReader outStream {
 
         return winSize, chkSum, urgPtr, payload |> Text.Encoding.UTF8.GetString
     }
-
---
+++ BitReaderWriterFs/BitReaderWriterFs.Examples/Simple.fsx
@@ -0,0 +1,27 @@
+﻿#r "bin/BitReaderWriterFs.dll"
+
+open System
+open BitReaderWriterFs
+
+open System.IO
+
+let stream = new MemoryStream()
+stream.WriteByte(255uy)
+stream.WriteByte(0uy)
+stream.WriteByte(129uy)
+stream.Write(Text.Encoding.ASCII.GetBytes("Hello World!"), 0, 12)
+
+stream.Position <- 0L
+
+let allT, allF, t, sixFOneT, bytes, world, ``!`` = bitReader stream {
+        let! allT     = BitReader.ReadInt32(4)
+        do! BitReader.Skip(8)
+        let! allF     = BitReader.ReadInt32(4)
+        let! t        = BitReader.ReadBool()
+        let! sixFOneT = BitReader.ReadInt32(7)
+        let! bytes   = BitReader.ReadBytes(6 * 8)  // "Hello "
+        let! world    = BitReader.ReadString(5 * 8)
+        let! ``!``    = BitReader.ReadChar()
+
+        return allT, allF, t, sixFOneT, bytes, world, ``!``
+    }
